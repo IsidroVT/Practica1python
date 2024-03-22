cc22e06fdaecbe298fa688ebb923282e31e23cf3
Isidro Valtueña <isidrovaltu04@gmail.com>
2024-03-22 20:47:40 -0300

    Se agregaron niveles de dificultad

--- game.py
+++ game.py
@@ -7,13 +7,31 @@ secret_word = random.choice(words)
 # Número máximo de fallos permitidos
 max_errors = 3
 errors = 0 # Variable que cuenta los fallos del jugador
+vowels = "aeiou"
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
 print("¡Bienvenido al juego de adivinanzas!")
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 word_displayed = "_" * len(secret_word)
-# Mostrarla palabra parcialmente adivinada
-print(f"Palabra: {word_displayed}")
+valid_difficulty = 0 # Variable usada para asegurar que se elija una dificultad valida
+# Eleccion de dificultad
+while valid_difficulty == 0:
+    difficulty = input("Elija nivel de dificultad(facil, medio, dificil): ").lower()
+    if difficulty == "facil":
+        guessed_letters = ["a", "e", "i", "o", "u"] # Agrega las vocales a las letras adivinadas
+        word_displayed = "".join(char if char in vowels else "_" for char in secret_word) # Muestra solo las vocales de la palabra
+        print(f"Palabra: {word_displayed}")
+        valid_difficulty = 1
+    elif difficulty == "medio":
+        word_displayed = f"{secret_word[0]}{'_' * (len(secret_word) - 2)}{secret_word[-1]}" # Muestra solo la 1ra y ultima letra de la palabra
+        guessed_letters = [secret_word[0], secret_word[-1]] 
+        print(f"Palabra: {word_displayed}")
+        valid_difficulty = 1
+    elif difficulty == "dificil":
+        print(f"Palabra: {word_displayed}")
+        valid_difficulty = 1
+    else:
+        print("No se ha elegido dificultad, vuelva a intentar")
 while max_errors != errors:
      # Pedir al jugador que ingrese una letra
     letter = input("Ingresa una letra: ").lower()
